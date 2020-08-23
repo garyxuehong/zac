0f051a00ccfe51ab4285fd51e7e2011bb1bac969
garyxuehong <gary.xuehong@gmail.com>
2020-08-23 17:25:39 +1000

    There are lots of 一s.
    more 一s

--- coin/coino.js
+++ coin/coino.js
@@ -1,7 +1,7 @@
 console.log(coin);
 var buttons = coin.map(c=>{
 	var button = document.createElement('button');
-	button.innerText = 'button 一一一一一一 '+c;
+	button.innerText = 'button 一一一一一一一一一 '+c;
 	button.addEventListener('click', ()=>{
 		console.log(c);
 	})
